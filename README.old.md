74f326c696a6f7c9e1d100b5c8778f2facde7fbd
playsoksana <playsoksana@gmail.com>
2021-10-24 21:47:20 +0300

    start work

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# MyFilmoteka
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import style from "./Header.module.scss";
+
+const Header = () => {
+  return <header className={style.Header}>Header</header>;
+};
+
+export default Header;
